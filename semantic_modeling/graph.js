099ab2872e13e878ccea22b5fb1553caf14fbd92
Giuseppe Futia <giuseppe.futia@gmail.com>
2018-06-05 14:51:57 +0200

    Integration of semantic types in the graph

--- semantic_modeling/graph.js
+++ semantic_modeling/graph.js
@@ -12,14 +12,13 @@ var relations = function (c_i, c_j, o) {
 
 }
 
-var add_semantic_types = function (graph) {
-    // Load semantic types
-
+var add_semantic_types = function (path, graph) {
+    // Load semantic types from file
+    var types = JSON.parse(fs.readFileSync(path, 'utf8'));
     // For each semantic type, you add a class node, a data node, and a link with label and weight
     for (var t in types) {
-
+        graph = create_semantic_types_nodes(types[t], graph)
     }
-
     return graph;
 }
 
@@ -37,7 +36,6 @@ var create_semantic_types_nodes = function (st, graph) {
         graph.setNode(data_node);
         graph.setEdge(class_node, data_node, { label: semantic_types[i][0].split("_")[1], weight: 1 });
     }
-    //console.log(graph);
     return graph;
 }
 
@@ -49,25 +47,24 @@ var connect_class_nodes = function () {
 
 }
 
-var buildGraph = function (path) {
+var buildGraph = function (st_path, ont_path) {
     var g = new Graph();
-
-    // TODO: Add semantic types
+    grap_with_semantic_types = add_semantic_types(st_path, g);
 
     // TODO: Get closures and relations according to the semantic types
 
     // Load domain ontology
     rdfstore.create(function (err, store) {
-        var ontology = fs.readFileSync(path).toString();
+        var ontology = fs.readFileSync(ont_path).toString();
         store.load('text/turtle', ontology, function (err, data) {
             if (err) {
                 console.log('Can not load the ontology!');
                 return;
             }
-            console.log('Ontology is loaded!');
         });
     });
 }
 
 exports.create_semantic_types_nodes = create_semantic_types_nodes;
+exports.add_semantic_types = add_semantic_types;
 exports.buildGraph = buildGraph;
