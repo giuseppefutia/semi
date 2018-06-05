f4f91ea5a2cae1752cbda4cfb9c596a5953af13c
Giuseppe Futia <giuseppe.futia@gmail.com>
2018-06-05 18:46:14 +0200

    Implement and test closure function for super classes

--- semantic_modeling/graph.js
+++ semantic_modeling/graph.js
@@ -2,10 +2,19 @@ var Graph = require('graphlib').Graph;
 var rdfstore = require('rdfstore');
 var fs = require('fs');
 
-var closure = function (c, o) {
-    var closure_classes = [];
-    // SPARQL query to get closure
-    return closure_classes;
+var PREFIX = {
+	'http://schema.org/': 'schema:',
+	'http://www.w3.org/2000/01/rdf-schema\#>': "rdfs:"
+}
+
+var get_class_nodes = function (graph) {
+    var class_nodes = [];
+    var nodes = graph.nodes();
+    for (var n in nodes) {
+        if (graph.node(nodes[n])['type'] === 'class_node')
+            class_nodes.push(nodes[n]);
+    }
+    return class_nodes;
 }
 
 var relations = function (c_i, c_j, o) {
@@ -28,19 +37,41 @@ var create_semantic_types_nodes = function (st, graph) {
     var semantic_types = st['semantic_types'];
     for (var i in attributes) {
         // Add class node
-        // TODO: check if class node already exists in the graph
+        // TODO: check if a class node already exists in the graph.
         var class_node = semantic_types[i][0].split("_")[0];
-        graph.setNode(class_node);
+        graph.setNode(class_node, {type: 'class_node'});
         // Add data node
         var data_node = attributes[i];
-        graph.setNode(data_node);
+        graph.setNode(data_node, {type: 'data_node'});
         graph.setEdge(class_node, data_node, { label: semantic_types[i][0].split("_")[1], weight: 1 });
     }
     return graph;
 }
 
-var add_closure = function (graph) {
+var add_closure = function (closure_classes, graph) {
+    // Closure classes are retrieved with SPARQL queries on the ontology
+    // TODO: check if a class node already exists in the graph. You can write a unique function.
+    for (var c in closure_classes) {
+        graph.setNode(closure_classes[c])
+    }
+}
 
+var get_closures = function (closure_query, store, cb) {
+    // For a class node in the graph, retrieve all super classes in the domain ontology
+    // Make SPARQL query
+    store.execute(closure_query, function (success, results) {
+        // Process query results
+        var closure_classes = [];
+        for (var c in results) {
+            var query_result = results[c]['super_classes']['value']; // super_classes is a protected word for the query
+            for (var p in PREFIX) {
+                if (query_result.indexOf(p) !== -1)
+                    query_result = query_result.replace(p, PREFIX[p]);
+            }
+            closure_classes.push(query_result);
+        }
+        cb(closure_classes);
+    });
 }
 
 var connect_class_nodes = function () {
@@ -61,10 +92,20 @@ var buildGraph = function (st_path, ont_path) {
                 console.log('Can not load the ontology!');
                 return;
             }
+            // Get closures
+
+
+
+
+            // Superclasses
+
         });
     });
 }
 
-exports.create_semantic_types_nodes = create_semantic_types_nodes;
+// Export for testing
+exports.get_class_nodes = get_class_nodes;
 exports.add_semantic_types = add_semantic_types;
+exports.create_semantic_types_nodes = create_semantic_types_nodes;
+exports.get_closures = get_closures;
 exports.buildGraph = buildGraph;
