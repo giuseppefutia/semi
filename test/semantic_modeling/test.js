4b2d010cbf14511de58ad4933b380a08a2e272f2
Giuseppe Futia <giuseppe.futia@gmail.com>
2018-06-05 14:51:57 +0200

    Integration of semantic types in the graph

--- test/semantic_modeling/test.js
+++ test/semantic_modeling/test.js
@@ -24,14 +24,29 @@ describe('Create semantic types nodes of a single data source', function () {
     before(function () {
         updated_graph = graph_generator.create_semantic_types_nodes(st, graph);
     });
-    it('The graph should have at least the following nodes: schema:Person, schema:identifier, schema:legalName', function () {
+    it('The graph should have at least the following nodes: schema:Person, identifier, affiliation', function () {
         assert.deepEqual(true, updated_graph.hasNode('schema:Person'));
+        assert.deepEqual(true, updated_graph.hasNode('identifier'));
+        assert.deepEqual(true, updated_graph.hasNode('affiliation'));
     });
 
 });
 
+describe('Add semantic types from different data sources', function () {
+    var graph = new Graph();
+    var updated_graph;
+    before(function () {
+        updated_graph = graph_generator.add_semantic_types(__dirname + '/semantic_types_test.json', graph)
+    });
+    it('The graph should have at least the following nodes: schema:Person, identifier, affiliation', function () {
+        assert.deepEqual(true, updated_graph.hasNode('schema:Person'));
+        assert.deepEqual(true, updated_graph.hasNode('identifier'));
+        assert.deepEqual(true, updated_graph.hasNode('affiliation'));
+    });
+});
+
 describe('Test on graph.js', function () {
     it('Graph should be built', function () {
-        graph_generator.buildGraph(__dirname + '/schema.ttl');
+        graph_generator.buildGraph(__dirname + '/semantic_types_test.json', __dirname + '/schema.ttl');
     });
 });
