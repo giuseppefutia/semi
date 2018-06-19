09c25f757df614b122195c7058617c347174f4a3
Giuseppe Futia <giuseppe.futia@gmail.com>
2018-06-19 12:49:32 +0200

    Add basic tests for elastic

--- semantic_typing/elastic_client.js
+++ semantic_typing/elastic_client.js
@@ -1,16 +1,22 @@
 var elasticsearch = require('elasticsearch');
+const config = require('../config');
+
 var client = new elasticsearch.Client({
-    host: 'localhost:9200',
-    log: 'trace'
+    host: config['dev']['elastic']['host'] + ':' + config['dev']['elastic']['port'],
+    log: config['dev']['elastic']['log']
 });
 
-client.ping({
-    // ping usually has a 3000ms timeout
-    requestTimeout: 1000
-}, function(error) {
-    if (error) {
-        console.trace('elasticsearch cluster is down!');
-    } else {
-        console.log('All is well');
-    }
-});
+var ping = function(c, cb) {
+    c.ping({
+        // ping usually has a 3000ms timeout
+    }, function(error) {
+        if (error) {
+            cb('elasticsearch cluster is down!');
+        } else {
+            cb('All is well')
+        }
+    });
+}
+
+// Export for testing
+exports.ping = ping;
