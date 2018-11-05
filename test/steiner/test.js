71643847314a52c5024b4bfdf1511cc3bf12dd63
Giuseppe Futia <giuseppe.futia@gmail.com>
2018-11-05 12:30:24 +0100

    Fix algorithm adding inverse edges

--- test/steiner/test.js
+++ test/steiner/test.js
@@ -8,49 +8,50 @@ var graph_generator = require(__dirname + '/../../semantic_modeling/graph.js');
 
 describe('Steiner algorithm test suite\n', function() {
 
-    describe('Test the algorithm', function() {
+    describe('Test creation of inverse edges', function() {
+        var graph = new Graph({
+            multigraph: true,
+        });
+        graph.setEdge('V9', 'V1', {
+            label: 'property_label',
+            type: 'property'
+        }, 'V9---V1', 1);
+        graph.setEdge('V9', 'V8', {
+            type: 'property',
+            label: 'property_label'
+        }, 'V9---V8', 0.5);
+        graph.setEdge('V8', 'V7', {
+            type: 'property',
+            label: 'property_label'
+        }, 'V8---V7', 0.5);
+
+        var new_graph = steiner.create_inverse_edges(graph);
+
+        it('It should crate the following edges: V1---V9', function() {
+            assert.deepEqual(new_graph.edges()[3]['name'], 'V1---V9');
+        });
+    });
+
+    describe('Test Steiner tree algorithm with a general graph', function() {
         // Algorithm tested with the example available here: https://www.researchgate.net/publication/227056882_A_Fast_Algorithm_for_Steiner_Trees.
         var graph = new Graph({
             multigraph: true,
         });
 
-        // Since it is an undirected graph, I have to represent both-directional edges
-        // Bellman-Ford works only with directed graphs, so I need to specify this kind of edges for testing
-        graph.setEdge('V1', 'V9', {}, 'V1---V9', 1);
         graph.setEdge('V9', 'V1', {}, 'V9---V1', 1);
-
         graph.setEdge('V9', 'V8', {}, 'V9---V8', 0.5);
-        graph.setEdge('V8', 'V9', {}, 'V8---V9', 0.5);
-
         graph.setEdge('V8', 'V7', {}, 'V8---V7', 0.5);
-        graph.setEdge('V7', 'V8', {}, 'V7---V8', 0.5);
-
         graph.setEdge('V7', 'V6', {}, 'V7---V6', 1);
-        graph.setEdge('V6', 'V7', {}, 'V6---V7', 1);
-
         graph.setEdge('V6', 'V5', {}, 'V6---V5', 1);
-        graph.setEdge('V5', 'V6', {}, 'V5---V6', 1);
-
         graph.setEdge('V9', 'V5', {}, 'V9---V5', 1);
-        graph.setEdge('V5', 'V9', {}, 'V5---V9', 1);
-
         graph.setEdge('V5', 'V4', {}, 'V5---V4', 2);
-        graph.setEdge('V4', 'V5', {}, 'V4---V5', 2);
-
         graph.setEdge('V4', 'V3', {}, 'V4---V3', 9);
-        graph.setEdge('V3', 'V4', {}, 'V3---V4', 9);
-
         graph.setEdge('V5', 'V3', {}, 'V5---V3', 2);
-        graph.setEdge('V3', 'V5', {}, 'V3---V5', 2);
-
         graph.setEdge('V3', 'V2', {}, 'V3---V2', 8);
-        graph.setEdge('V2', 'V3', {}, 'V2---V3', 8);
-
         graph.setEdge('V2', 'V6', {}, 'V2---V6', 1);
-        graph.setEdge('V6', 'V2', {}, 'V6---V2', 1);
-
         graph.setEdge('V1', 'V2', {}, 'V1---V2', 10);
-        graph.setEdge('V2', 'V1', {}, 'V2---V1', 10);
+
+        var graph = steiner.create_inverse_edges(graph);
 
         var s = ['V1', 'V2', 'V3', 'V4'];
 
@@ -59,70 +60,59 @@ describe('Steiner algorithm test suite\n', function() {
         var G3 = steiner.step_three(G2, graph);
         var G4 = steiner.step_four(G3);
         var G5 = steiner.step_five(G4, s);
-        console.log('Steiner Tree of a general graph: ');
-        console.log(graphlib.json.write(G5));
-        console.log('\n');
-    });
 
-    describe('Test with semantic types', function() {
-        var graph = new Graph({
-            multigraph: true,
+        var test_edges = [{
+                v: 'V1',
+                w: 'V9',
+                name: 'V1_V9',
+                value: {},
+                weight: 1
+            },
+            {
+                v: 'V2',
+                w: 'V6',
+                name: 'V2_V6',
+                value: {},
+                weight: 1
+            },
+            {
+                v: 'V3',
+                w: 'V5',
+                name: 'V3_V5',
+                value: {},
+                weight: 2
+            },
+            {
+                v: 'V5',
+                w: 'V4',
+                name: 'V5_V4',
+                value: {},
+                weight: 2
+            },
+            {
+                v: 'V6',
+                w: 'V5',
+                name: 'V6_V5',
+                value: {},
+                weight: 1
+            },
+            {
+                v: 'V9',
+                w: 'V5',
+                name: 'V9_V5',
+                value: {},
+                weight: 1
+            }
+        ];
+
+        it('Test Steiner tree algorithm with public contracts data', function() {
+            assert.deepEqual(graphlib.json.write(G5)['edges'], test_edges);
         });
-
-        var sampleData = [{
-            'source': 'data/schema.csv',
-            'attributes': ['identifier', 'name', 'birth_date'], // Removed affiliation from starting sample
-            'semantic_types': [
-                ['schema:Person_schema:identifier'],
-                ['schema:Person_schema:name'], // Removed ['schema:Organization_schema:legalName,'] from starting sample
-                ['schema:Person_schema:birth_date']
-            ],
-            'scores': [
-                [0.5],
-                [0.7],
-                [0.1],
-                [0.4]
-            ]
-        }];
-
-        var attributes = sampleData[0]['attributes'];
-        var semantic_types = sampleData[0]['semantic_types'];
-
-        for (var i in attributes) {
-            // Add class node
-            // TODO: check if a class node already exists in the graph.
-            var class_node = semantic_types[i][0].split('_')[0];
-            graph.setNode(class_node, {
-                type: 'class_uri'
-            });
-            // Add data node
-            var data_node = attributes[i];
-            graph.setNode(data_node, {
-                type: 'attribute_name'
-            });
-            // Add edge
-            graph.setEdge(class_node, data_node, {
-                label: semantic_types[i][0].split('_')[1],
-                type: 'property_uri'
-            }, semantic_types[i][0].split('_')[1], 1);
-        }
-
-        // TODO: Understand why I need to add this edge
-        graph.setEdge('identifier', 'schema:Person', {}, 'test', 3);
-
-        var steiner_nodes = attributes;
-        var G1 = steiner.step_one(graph, steiner_nodes);
-        var G2 = steiner.step_two(G1);
-        var G3 = steiner.step_three(G2, graph);
-        var G4 = steiner.step_four(G3);
-        var G5 = steiner.step_five(G4, steiner_nodes);
-        console.log('Steiner Tree of a RDF-similar and weighted graph: ');
-        console.log(graphlib.json.write(G5));
     });
 
     describe('Test with other semantic types (more similar to our case)', function() {
         var graph = new Graph({
-            multigraph: true,
+            multigraph: true
         });
 
         var sampleData = {
@@ -134,11 +124,71 @@ describe('Steiner algorithm test suite\n', function() {
                 ['gr:BusinessEntity_dcterms:identifier']
             ],
         };
-
+        // Add semantic types
         graph_generator.add_semantic_types(sampleData, graph);
-        console.log('******* See this test **********');
-        console.log(graphlib.json.write(graph));
 
+        // Add edges between semantic types
+        graph.setEdge('pc:Contract0', 'gr:BusinessEntity0', {}, 'pc:Contract0***gr:BusinessEntity0', 1);
+        graph.setEdge('pc:Contract0', 'gr:BusinessEntity1', {}, 'pc:Contract0***gr:BusinessEntity1', 5);
+        graph.setEdge('pc:Contract0', 'pc:Tender0', {}, 'pc:Contract0***pc:Tender0', 1);
+        graph.setEdge('pc:Tender0', 'gr:BusinessEntity1', {}, 'pc:Tender0***gr:BusinessEntity1', 1);
 
+        // Add inverse edges
+        var graph = steiner.create_inverse_edges(graph);
+
+        var steiner_nodes = sampleData['attributes'];
+        var G1 = steiner.step_one(graph, steiner_nodes);
+        var G2 = steiner.step_two(G1);
+        var G3 = steiner.step_three(G2, graph);
+        var G4 = steiner.step_four(G3);
+        var G5 = steiner.step_five(G4, steiner_nodes);
+
+        var test_edges = [{
+                v: 'authority_identifier',
+                w: 'gr:BusinessEntity0',
+                name: 'authority_identifier_gr:BusinessEntity0',
+                value: {},
+                weight: 1
+            },
+            {
+                v: 'contract_identifier',
+                w: 'pc:Contract0',
+                name: 'contract_identifier_pc:Contract0',
+                value: {},
+                weight: 1
+            },
+            {
+                v: 'gr:BusinessEntity0',
+                w: 'pc:Contract0',
+                name: 'gr:BusinessEntity0_pc:Contract0',
+                value: {},
+                weight: 1
+            },
+            {
+                v: 'gr:BusinessEntity1',
+                w: 'business_entity_identifier',
+                name: 'gr:BusinessEntity1_business_entity_identifier',
+                value: {},
+                weight: 1
+            },
+            {
+                v: 'pc:Contract0',
+                w: 'pc:Tender0',
+                name: 'pc:Contract0_pc:Tender0',
+                value: {},
+                weight: 1
+            },
+            {
+                v: 'pc:Tender0',
+                w: 'gr:BusinessEntity1',
+                name: 'pc:Tender0_gr:BusinessEntity1',
+                value: {},
+                weight: 1
+            }
+        ];
+
+        it('The graph should have the edges defined in the test', function() {
+            assert.deepEqual(graphlib.json.write(G5)['edges'], test_edges);
+        });
     });
 });
