5dd85a794285ebaddb1a2546c679f81214f3de99
Giuseppe Futia <giuseppe.futia@gmail.com>
2018-11-22 12:27:54 +0100

    Move running scripts externally

--- semantic_modeling/jarql.js
+++ semantic_modeling/jarql.js
@@ -115,21 +115,6 @@ var build_jarql = (semantic_types) => {
     return jarql_string;
 }
 
-var run = () => {
-    if (arguments.length !== 5) {
-        console.log('Number of arguments is wrong!');
-        return;
-    }
-    var st_path = process.argv.slice(2)[0];
-    var sm_path = process.argv.slice(3)[0];
-    var st = JSON.parse(fs.readFileSync(st_path))[0];
-    var jarql_to_print = build_jarql(st);
-    fs.writeFileSync(sm_path, jarql_to_print);
-    console.log('JARQL file is written in: ' + sm_path);
-}
-
-run();
-
 // Export for testing
 exports.build_prefix = build_prefix;
 exports.build_construct = build_construct;
