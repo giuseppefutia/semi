848bd691810d60964f5192acaf7d583a9e55af1b
Giuseppe Futia <giuseppe.futia@gmail.com>
2019-04-04 14:23:28 +0200

    Add JSON data for RODI

---
+++ data/rodi/cmt2conference/scripts/graph.sh
@@ -0,0 +1,1 @@
+node run/graph.js data/pc/semantic_types/Z4ADEA9DE4_st.json data/pc/ontology/public-contracts.ttl rdfs:domain rdfs:range owl:Class data/pc/semantic_models/Z4ADEA9DE4
---
+++ data/rodi/cmt2conference/scripts/rdf.sh
@@ -0,0 +1,1 @@
+java -jar jarql-1.0.1-SNAPSHOT.jar data/rodi/cmt2conference/input/data.json data/rodi/cmt2conference/gold/semantic_models/data.query > data/rodi/cmt2conference/output/data.rdf
