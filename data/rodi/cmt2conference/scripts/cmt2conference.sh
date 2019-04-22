f22cebc86ec7e0c407c007c0d569dc3820469bbc
Giuseppe Futia <giuseppe.futia@gmail.com>
2019-04-22 20:27:00 +0200

    [Feature] Update single scenario with input, output and scripts

--- data/rodi/cmt2conference/scripts/cmt2conference.sh
+++ data/rodi/cmt2conference/scripts/cmt2conference.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
-
-./data/rodi/cmt2conference/scripts/cmt2conference_graph.sh
-./data/rodi/cmt2conference/scripts/cmt2conference_steiner.sh
-./data/rodi/cmt2conference/scripts/cmt2conference_jarql.sh
-./data/rodi/cmt2conference/scripts/cmt2conference_rdf.sh
+sudo chmod u+x data/rodi/cmt2conference/scripts/ 
+data/rodi/cmt2conference/scripts/cmt2conference_graph.sh
+data/rodi/cmt2conference/scripts/cmt2conference_steiner.sh
+data/rodi/cmt2conference/scripts/cmt2conference_jarql.sh
+data/rodi/cmt2conference/scripts/cmt2conference_rdf.sh
