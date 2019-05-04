01bb3732d06c95c505459d185b25ad096ed84574
Giuseppe Futia <giuseppe.futia@gmail.com>
2019-05-04 12:02:59 +0200

    [Data] Update cmt_denormalized

--- data/rodi/cmt_denormalized/scripts/cmt_denormalized.sh
+++ data/rodi/cmt_denormalized/scripts/cmt_denormalized.sh
@@ -3,3 +3,4 @@ data/rodi/cmt_denormalized/scripts/cmt_denormalized_graph.sh
 data/rodi/cmt_denormalized/scripts/cmt_denormalized_steiner.sh
 data/rodi/cmt_denormalized/scripts/cmt_denormalized_jarql.sh
 data/rodi/cmt_denormalized/scripts/cmt_denormalized_rdf.sh
+cat data/rodi/cmt_denormalized/output/* 
