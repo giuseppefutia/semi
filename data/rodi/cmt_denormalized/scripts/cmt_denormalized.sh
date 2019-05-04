dd3f6d9d6aedef5e7d478b0f13dea56f4900ccca
Giuseppe Futia <giuseppe.futia@gmail.com>
2019-05-04 10:59:30 +0200

    [Data] cmt_denormalized scenario

---
+++ data/rodi/cmt_denormalized/scripts/cmt_denormalized.sh
@@ -0,0 +1,5 @@
+sudo chmod u+x data/rodi/cmt_denormalized/scripts/* 
+data/rodi/cmt_denormalized/scripts/cmt_denormalized_graph.sh
+data/rodi/cmt_denormalized/scripts/cmt_denormalized_steiner.sh
+data/rodi/cmt_denormalized/scripts/cmt_denormalized_jarql.sh
+data/rodi/cmt_denormalized/scripts/cmt_denormalized_rdf.sh
