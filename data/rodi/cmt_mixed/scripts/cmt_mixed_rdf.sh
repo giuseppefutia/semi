36e8259aef26706e3fffe23fed42aa0ebb94c016
Giuseppe Futia <giuseppe.futia@gmail.com>
2019-05-07 10:42:37 +0200

    [Data] Update cmt_mixed

--- data/rodi/cmt_mixed/scripts/cmt_mixed_rdf.sh
+++ data/rodi/cmt_mixed/scripts/cmt_mixed_rdf.sh
@@ -1,4 +1,4 @@
-java -jar jarql-1.0.1-SNAPSHOT.jar data/rodi/cmt_mixed/input/cmt_mixed___co_write_paper.json data/rodi/cmt_mixed/semantic_models/cmt_mixed___co_write_paper.query > data/rodi/cmt_mixed/output/cmt_mixed___co_write_paper.rdf 
-java -jar jarql-1.0.1-SNAPSHOT.jar data/rodi/cmt_mixed/input/cmt_mixed___papers.json data/rodi/cmt_mixed/semantic_models/cmt_mixed___papers.query > data/rodi/cmt_mixed/output/cmt_mixed___papers.rdf 
-java -jar jarql-1.0.1-SNAPSHOT.jar data/rodi/cmt_mixed/input/cmt_mixed___person_conference.json data/rodi/cmt_mixed/semantic_models/cmt_mixed___person_conference.query > data/rodi/cmt_mixed/output/cmt_mixed___person_conference.rdf 
-java -jar jarql-1.0.1-SNAPSHOT.jar data/rodi/cmt_mixed/input/cmt_mixed___reviews.json data/rodi/cmt_mixed/semantic_models/cmt_mixed___reviews.query > data/rodi/cmt_mixed/output/cmt_mixed___reviews.rdf 
+java -jar jarql-1.0.1-SNAPSHOT.jar data/rodi/cmt_mixed/input/cmt_mixed___co_write_paper.json data/rodi/cmt_mixed/semantic_models/cmt_mixed___co_write_paper.query > data/rodi/cmt_mixed/output/cmt_mixed___co_write_paper.ttl 
+java -jar jarql-1.0.1-SNAPSHOT.jar data/rodi/cmt_mixed/input/cmt_mixed___papers.json data/rodi/cmt_mixed/semantic_models/cmt_mixed___papers.query > data/rodi/cmt_mixed/output/cmt_mixed___papers.ttl 
+java -jar jarql-1.0.1-SNAPSHOT.jar data/rodi/cmt_mixed/input/cmt_mixed___person_conference.json data/rodi/cmt_mixed/semantic_models/cmt_mixed___person_conference.query > data/rodi/cmt_mixed/output/cmt_mixed___person_conference.ttl 
+java -jar jarql-1.0.1-SNAPSHOT.jar data/rodi/cmt_mixed/input/cmt_mixed___reviews.json data/rodi/cmt_mixed/semantic_models/cmt_mixed___reviews.query > data/rodi/cmt_mixed/output/cmt_mixed___reviews.ttl 
