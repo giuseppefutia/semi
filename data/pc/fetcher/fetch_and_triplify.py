58e0d109091672c30526bcd9e1337e904ff0c9a9
Davide Allavena <davide.allavena@gmail.com>
2019-05-14 15:18:28 +0200

    Finalization

--- data/pc/fetcher/fetch_and_triplify.py
+++ data/pc/fetcher/fetch_and_triplify.py
@@ -3,11 +3,11 @@ import json
 import xmltodict
 import xml.etree.ElementTree as ET
 import os
-
+import random
 
 urls = [
     #"https://www.swas.polito.it/services/avcp/avcp_dataset_2015_annorif_2015.xml",
-    "https://www.swas.polito.it/services/avcp/avcp_dataset_2016_annorif_2016.xml",
+    #"https://www.swas.polito.it/services/avcp/avcp_dataset_2016_annorif_2016.xml",
     "https://www.swas.polito.it/services/avcp/avcp_dataset_2017_annorif_2017_1.xml",
     "https://www.swas.polito.it/services/avcp/avcp_dataset_2018_annorif_2018_1.xml",
     "https://www.unito.it/sites/default/files/legge190/2018/dataset_2018_00027.xml",
@@ -25,9 +25,11 @@ for url in urls:
     #print(JSONrecord)
     for lotto in JSONrecord["legge190:pubblicazione"]['data']['lotto']:
         #print(lotto)
+
         if "partecipanti" in lotto.keys():
             if lotto["partecipanti"] != None:
-                lotto["partecipanti"] = lotto["partecipanti"]["partecipante"]
+                    if "partecipante"  in lotto["partecipanti"].keys():
+                        lotto["partecipanti"] = lotto["partecipanti"]["partecipante"]
         if "aggiudicatari" in lotto.keys():
             if lotto["aggiudicatari"] != None:
                 if "aggiudicatario" in lotto["aggiudicatari"].keys():
@@ -35,9 +37,20 @@ for url in urls:
                 elif "aggiudicatario" in lotto["aggiudicatari"].keys():
                     lotto["aggiudicatari"] = lotto["aggiudicatari"]["aggiudicatarioRaggruppamento"]['membro']
 
-        filename = lotto['cig'] + ".json"
+        if random.randint(1,10) > 9:
 
-        with open(filename, 'w', encoding='utf-8') as tmp_file:
-            tmp_file.write(json.dumps(lotto, indent=2))
-        #json.dump(lotto, "file.json", indent=2)
-        os.system("java -jar jarql-1.0.0.jar "+ filename + " ../gold/semantic_models/Z4ADEA9DE4_sm.query >> final.ttl")
+            filename = lotto['cig'] + ".json"
+
+            with open(filename, 'w', encoding='utf-8') as tmp_file:
+                tmp_file.write(json.dumps(lotto, indent=2))
+            #json.dump(lotto, "file.json", indent=2)
+            os.system("java -jar jarql-1.0.0.jar "+ filename + " pc.query >> final.ttl")
+
+        else:
+
+            filename = lotto['cig'] + "-no-relation.json"
+
+            with open(filename, 'w', encoding='utf-8') as tmp_file:
+                tmp_file.write(json.dumps(lotto, indent=2))
+            #json.dump(lotto, "file.json", indent=2)
+            os.system("java -jar jarql-1.0.0.jar "+ filename + " pc-no_relation.query >> final.ttl")
