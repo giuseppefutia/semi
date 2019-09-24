67ac992f9edaa80c2ae8d71cd70158ce75131660
Giuseppe Futia <giuseppe.futia@gmail.com>
2019-09-24 18:25:56 +0200

    [Feature] Basic scripts to interact with elastic

--- src/semantic_typing/elastic_client.js
+++ src/semantic_typing/elastic_client.js
@@ -1,150 +1,46 @@
-var elasticsearch = require('elasticsearch');
+const es_client = require('./client');
 
-var create_client = function(host_name, port_value, log_level) {
-    return new elasticsearch.Client({
-        host: host_name + ':' + port_value,
-        log: log_level
-    })
-}
-
-var ping = function(c, cb) {
-    c.ping({
-        // ping usually has a 3000ms timeout
-    }, function(error) {
-        if (error) {
-            cb('elasticsearch cluster is down!');
-        } else {
-            cb('All is well');
-        }
+const index_exist = async function(index_name) {
+    return await es_client.indices.exists({
+        index: index_name
     });
 }
 
-var delete_all_indices = function(c) {
-    return new Promise(function(resolve, reject) {
-        c.indices.delete({
-            index: '_all'
-        }, function(err, res, status) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
+const create_index = async function(index_name) {
+    return await es_client.indices.create({
+        index: index_name
     });
 }
 
-var create_index = function(c, index_name) {
-    return new Promise(function(resolve, reject) {
-        c.indices.create({
-            index: index_name
-        }, function(err, res, status) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
+const add_mapping_to_index = async function(index_name, mapping_type, mapping) {
+    return await es_client.indices.putMapping({
+        index: index_name,
+        type: mapping_type,
+        body: mapping,
+        include_type_name: true
     });
 }
 
-var create_mapping = function(c, index_name, type_name, properties_value) {
-    return new Promise(function(resolve, reject) {
-        c.indices.putMapping({
-            index: index_name,
-            type: type_name,
-            body: {
-                properties: properties_value
-            }
-        }, function(err, res, status) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
+const insert_doc = async function(index_name, _id, mapping_type, data) {
+    return await es_client.index({
+        index: index_name,
+        type: mapping_type,
+        id: _id,
+        body: data
     });
 }
 
-var get_mapping = function (c, index_name, type_name) {
-    return new Promise(function(resolve, reject) {
-        c.indices.getMapping({
-            index: index_name,
-            type: type_name
-        }, function(err, res, status) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
+const search_doc = async function(index_name, mapping_type, payload) {
+    return await es_client.search({
+        index: index_name,
+        type: mapping_type,
+        body: payload
     });
 }
 
-var add_document_to_index = function(c, index_name, id_name, type_name, label_name, content_value) {
-    return new Promise(function(resolve, reject) {
-        c.index({
-            index: index_name,
-            id: id_name,
-            type: type_name,
-            body: {
-                'label': label_name,
-                'content': content_value,
-            },
-            refresh: true
-        }, function(err, res, status) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
-    });
-}
-
-// TODO
-var create_bulk = function() {
-
-}
-
-var search_in_index = function(c, index_name, type_name, query_value) {
-    return new Promise(function(resolve, reject) {
-        c.search({
-            index: index_name,
-            type: type_name,
-            body: {
-                query: query_value
-            }
-        }, function(err, res, status){
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
-    });
-}
-
-var drop_index = function(c, index_name) {
-    return new Promise(function(resolve, reject) {
-        c.indices.delete({
-            index: index_name
-        }, function(err, res, status) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
-            }
-        });
-    });
-}
-
-// Export for testing
-exports.create_client = create_client;
-exports.ping = ping;
-exports.delete_all_indices = delete_all_indices;
+// Export functions
+exports.index_exist = index_exist;
 exports.create_index = create_index;
-exports.create_mapping = create_mapping;
-exports.get_mapping = get_mapping;
-exports.add_document_to_index = add_document_to_index;
-exports.search_in_index = search_in_index;
-exports.drop_index = drop_index;
+exports.add_mapping_to_index = add_mapping_to_index;
+exports.insert_doc = insert_doc;
+exports.search_doc = search_doc;
