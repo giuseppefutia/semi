3bc28bdfa85a04c90e8c2fb7574c3b8d652732a8
Giuseppe Futia <giuseppe.futia@gmail.com>
2020-01-05 17:07:55 +0100

    [preparation] Advanced cleaning scripts for sources and semantic types

---
+++ preparation/taheriyan2016/clean.js
@@ -0,0 +1,257 @@
+var fs = require('fs');
+var utils = require('./clean_utils.js');
+var task = process.argv.slice(2)[0];
+
+var source_folder = 'data/taheriyan2016/' + task + '/sources/original_json/';
+var output_folder = 'data/taheriyan2016/' + task + '/sources/updated_json/';
+var st_folder = 'data/taheriyan2016/' + task + '/semantic_types/auto/';
+var st_output_folder = 'data/taheriyan2016/' + task + '/semantic_types/updated/';
+
+var sources = fs.readdirSync(source_folder);
+var sts = fs.readdirSync(st_folder);
+
+// Updates on sources and semantic types in task_01, task_02, task_03
+
+var clean_brackets = (source_name, source) => {
+    // Manage anomalies
+    if (source_name === 's16-s-hammer.json') {
+        source = source['collections']['collection'];
+        console.log('   Anomaly case in the source');
+        return source;
+    } else if (source_name === 's18-s-indianapolis-artists.json') {
+        source = source['artists']['artist'];
+        console.log('   Anomaly case in the source');
+        return source;
+    } else if (source_name === 's19-s-indianapolis-artworks.json') {
+        source = source['artworks']['artwork'];
+        console.log('   Anomaly case in the source');
+        return source;
+    } else if (source_name === 's27-s-the-huntington.json') {
+        source = source['drawings'];
+        console.log('   Anomaly case in the source');
+        return source;
+    }
+
+    var external_key = Object.keys(source)[0];
+    var internal_key = Object.keys(source[external_key])[0];
+    if (internal_key == 0) {
+        // Less deep object
+        source = source[external_key];
+    } else {
+        // More deep object
+        source = source[external_key][internal_key];
+    }
+    return source;
+}
+
+var create_uri_field = (source_name, source, field, uri_field) => {
+
+    // Manage anomalies in adding URIs
+
+    // Anomaly
+    if (source_name === 's16-s-hammer.json') {
+        for (var obj of source) {
+            for (var item of obj['item']) {
+                item[uri_field] = item[field];
+            }
+        }
+        console.log('\n   Add new field: ' + uri_field + ' \n   Get value from ' + field + ' field');
+        return;
+    }
+    // Anomaly
+    else if (source_name === 's26-s-san-francisco-moma.json') {
+        for (var obj of source) {
+            obj['art-work'][uri_field] = obj['art-work'][field];
+        }
+        console.log('\n   Add new field: ' + uri_field + ' \n   Get value from ' + field + ' field');
+        return;
+    }
+
+    // Default behaviour
+    else if (source[0][field] === undefined) return;
+    else if (source[0][uri_field] !== undefined) return;
+    for (var obj of source) {
+        obj[uri_field] = obj[field];
+    }
+    console.log('\n   Add new field: ' + uri_field + ' \n   Get value from ' + field + ' field');
+}
+
+var special_semantic_types_update = (st_name, st) => {
+    // s16-s-hammer_st.json'
+    if (st_name === 's16-s-hammer_st.json') {
+        var attrs = st[0]['attributes'];
+        var new_attrs = attrs.filter(i => {
+            return i !== 'name';
+        }).map(i => {
+            return 'item.' + i;
+        });
+        new_attrs.push('name');
+        st[0]['attributes'] = new_attrs;
+        console.log('\n   Semantic type issue: ');
+        console.log('   There is an array in the source: add string "item". to the semantic type attributes');
+    }
+    // s26-s-san-francisco-moma_st.json
+    else if (st_name === 's26-s-san-francisco-moma_st.json') {
+        var attrs = st[0]['attributes'];
+        new_attrs = attrs.map(i => {
+            return 'art-work.' + i;
+        });
+        st[0]['attributes'] = new_attrs;
+        console.log('\n   Semantic type issue: ');
+        console.log('   There is an array in the source: add string "artwork". to the semantic type attributes');
+    }
+    // s27-s-the-huntington_st.json
+    else if (st_name === 's27-s-the-huntington_st.json') {
+        var attrs = st[0]['attributes'];
+        new_attrs = attrs.map(i => {
+            if (i === 'nationality' || i === 'name' || i === 'birth_date' || i === 'death_date') {
+                return 'artist.' + i;
+            } else if (i === 'dimensions_inch') {
+                return 'dimensions.' + i;
+            } else return i;
+        });
+        st[0]['attributes'] = new_attrs;
+        console.log('\n   Semantic type issue');
+    }
+}
+
+var replace_classLink = (st) => {
+    var semantic_types = st[0]['semantic_types'];
+    var to_replace = 'http://isi.edu/integration/karma/dev#classLink';
+    st[0]['semantic_types'] = semantic_types.map(i => {
+        if (i[0].indexOf(to_replace) !== -1) {
+            return [i[0].replace(to_replace, 'rdfs:label')];
+        }
+        return i;
+    });
+}
+
+var clean_task_two = (a, attrs, source_name, source, attr_index, st) => {
+
+    // Include non-trivial semantic type fields in sources
+    utils.TASK_02_NEW_FIELDS[source_name].forEach(f => {
+        // Anomaly
+        if (source_name === 's26-s-san-francisco-moma.json') {
+            for (var obj of source) {
+                obj['art-work'][Object.keys(f)[0]] = obj['art-work'][Object.values(f)[0]];
+            }
+        }
+        // Anomaly
+        else if (source_name === 's16-s-hammer.json') {
+            for (var obj of source) {
+                for (var item of obj['item']) {
+                    item[Object.keys(f)[0]] = item[Object.values(f)[0]];
+                }
+            }
+        }
+        // Anomaly
+        else if (source_name === 's27-s-the-huntington.json') {
+            for (var obj of source) {
+                obj['Death_URI'] = obj['artist']['death_date'];
+                obj['artist_appellation_uri'] = obj['artist']['name'];
+                obj['object_uri'] = obj['object_no'];
+                obj['Nationality_URI'] = obj['artist']['nationality'];
+                obj['Birth_URI'] = obj['artist']['birth_date'];
+                obj['artist_URI'] = obj['artist']['name'];
+            }
+        }
+        // Default behaviour
+        else {
+            for (var obj of source) {
+                obj[Object.keys(f)[0]] = obj[Object.values(f)[0]];
+            }
+        }
+    });
+}
+
+sources.forEach((source_name, index) => {
+    var source_path = source_folder + source_name;
+    var output_path = output_folder + source_name;
+    var st_path = st_folder + sts[index];
+    var st_output_path = st_output_folder + sts[index];
+
+    console.log('\nProcessing source: ' + source_name + ' of ' + task + '...');
+
+    if (source_name === 's21-s-met.json') {
+        console.log('\n   Skip invalid file: ' + source_name);
+        return;
+    }
+    var source = JSON.parse(fs.readFileSync(source_path, 'utf-8'));
+
+    console.log('\n   Get semantic type: ' + sts[index]);
+    var st = JSON.parse(fs.readFileSync(st_path, 'utf-8'));
+    var attrs = st[0]['attributes'];
+
+    // Clean redundant brackets
+    if (utils.SOURCE_TO_CLEAN_BRACKETS[source_name] !== undefined) {
+        console.log('\n   Clean brackets...');
+        source = clean_brackets(source_name, source);
+    }
+
+    // Get special attributes from the semantic types
+    var values_index = 0; // Useful to map "values" field of semantic types to the correct field in the source "
+    attrs.forEach((a, attr_index) => {
+
+        // Check the following cases : '_uri', ' uri', 'uri'
+        if (a.match(/_uri/gi) !== null) {
+            var field = a.replace(/_uri/gi, '');
+            create_uri_field(source_name, source, field, a);
+
+        } else if (a.match(/ uri/gi) !== null) {
+            var field = a.replace(/ uri/gi, '');
+            create_uri_field(source_name, source, field, a);
+
+        } else if (a.match(/uri/gi) !== null) {
+            var field = a.replace(/uri/gi, '');
+            create_uri_field(source_name, source, field, a);
+        }
+
+        if (task === 'task_02') {
+            // Check the 'value' cases
+            if (a === 'values') {
+                // Replace 'values' in semantic types with the related field in the source
+                var field = utils.TASK_02_VALUES[source_name][values_index];
+                attrs[attr_index] = field;
+                console.log('\n   Replace "values" with: ' + field);
+                st[0]['attributes'] = attrs;
+                values_index++;
+            }
+            clean_task_two(a, attrs, source_name, source, values_index, attr_index, st);
+        }
+    });
+    // Special updates on semantic types
+    special_semantic_types_update(sts[index], st);
+
+    // Replace http://isi.edu/integration/karma/dev#classLink with rdfs:label
+    replace_classLink(st);
+
+    // Raise a warning message if there is no correspondance in uris in sources and semantic types
+    var uri_in_sources = Object.keys(source[0]).filter(i => {
+        return i.match(/uri/gi);
+    });
+
+    var uri_in_st_attributes = st[0]['attributes'].filter(i => {
+        return i.match(/uri/gi);
+    })
+
+    if (uri_in_sources.length !== uri_in_st_attributes.length) {
+        console.log('\n   *** WARNING: the number of uris is different! ***');
+        console.log('   ' + uri_in_sources.length + ' vs ' + uri_in_st_attributes.length);
+        console.log('    In source: ' + uri_in_sources.sort())
+        console.log('    In semantic type: ' + uri_in_st_attributes.sort())
+    }
+
+    // Raise a Warning message if the number of attributes in the semantic type and in the source are different
+    /*    var source_length = Object.keys(source[0]).length;
+        var attrs_length = st[0]['attributes'].length
+        if (source_length !== attrs_length) {
+            console.log('\n   *** WARNING: the number of fields and attributes is different! ***');
+            console.log('   ' + source_length + ' vs ' + attrs_length);
+        }*/
+
+    // Print updated sources and semantic types
+    fs.writeFileSync(output_path, JSON.stringify(source, null, 4));
+    fs.writeFileSync(st_output_path, JSON.stringify(st, null, 4));
+
+    console.log('\n... end of processing of ' + source_name + '.\n\n');
+});
---
+++ preparation/taheriyan2016/clean_utils.js
@@ -0,0 +1,305 @@
+module.exports.SOURCE_TO_CLEAN_BRACKETS = {
+    's03-ima-artists.json': 1,
+    's04-ima-artworks.json': 1,
+    's08-s-17-edited.json': 1,
+    's11-s-19-artists.json': 1,
+    's12-s-19-artworks.json': 1,
+    's13-s-art-institute-of-chicago.json': 1,
+    's14-s-california-african-american.json': 1,
+    's15-s-detroit-institute-of-art.json': 1,
+    's16-s-hammer.json': 1,
+    's18-s-indianapolis-artists.json': 1,
+    's19-s-indianapolis-artworks.json': 1,
+    's20-s-lacma.json': 1,
+    's22-s-moca.json': 1,
+    's26-s-san-francisco-moma.json': 1,
+    's27-s-the-huntington.json': 1,
+};
+
+module.exports.TASK_02_VALUES = {
+    's05-met.json': ['What', 'Dimensions'],
+    's09-s-18-artists.json': ['name', 'name'],
+    's10-s-18-artworks.json': ['description', 'accession_id', 'inscription', 'title', 'accession_id', 'dimensions', 'materials', 'collection_name', 'collection_name', 'artist', 'credit'],
+    's23-s-national-portrait-gallery.json': ['Sitter']
+};
+
+module.exports.TASK_02_NEW_FIELDS = {
+    's01-cb.json': [{
+        'Actor_URI': 'Attribution'
+    }, {
+        'Death_URI': 'End Date'
+    }, {
+        'Birth_URI': 'Begin Date'
+    }, {
+        'Artist_Appellation_URI': 'Attribution'
+    }],
+    's02-dma.json': [{
+        'Artist URI': 'Artist Name'
+    }, {
+        'Nationality_URI': 'Artist Nationality'
+    }, {
+        'Death_URI': 'Artist Death Date'
+    }, {
+        'Museum URI': 'Object Link Source'
+    }, {
+        'Birth_URI': 'Artist Birth Date'
+    }, {
+        'Object_URI': 'Object ID'
+    }, {
+        'Medium URI': 'Object Facet Value 1'
+    }],
+    's03-ima-artists.json': [{
+        'Birth_URI': 'birthDeath'
+    }, {
+        'Artist URI': 'name'
+    }, {
+        'Death_URI': 'birthDeath'
+    }, {
+        'artist appellation uri': 'name'
+    }],
+    's04-ima-artworks.json': [{
+        'Death_URI': 'birthDeath'
+    }, {
+        'galleryLabelTypeURI': 'galleryLabel'
+    }, {
+        'artistAppellationURI': 'artist'
+    }, {
+        'Birth_URI': 'birthDeath'
+    }, {
+        'provenanceTypeURI': 'provenance'
+    }],
+    's05-met.json': [{
+        'Person URI': 'Who'
+    }, {
+        'Object URI': 'id'
+    }, {
+        'ProvenanceTypeURI': 'Accession Number'
+    }, {
+        'Appellation URI': 'Who'
+    }],
+    's06-npg.json': [{
+        'ArtistAppellation_URI': 'Nahum Ball Onthank'
+    }, {
+        'Birth_URI': 'ArtistBornDiedDate'
+    }, {
+        'Death_URI': 'ArtistBornDiedDate'
+    }, {
+        'ObjectURI': 'Ref'
+    }],
+    's07-s-13.json': [{
+        'Birth_URI': 'birth_and_death_years'
+    }, {
+        'technique_uri': 'technique'
+    }, {
+        'Death_URI': 'birth_and_death_years'
+    }, {
+        'label_type_uri': 'ac_id'
+    }, {
+        'Object_URI': 'ac_id'
+    }, {
+        'origin_type_uri': 'ac_id'
+    }, {
+        'Artist_appellation_uri': 'name'
+    }, {
+        'Artist_uri': 'name'
+    }],
+    's08-s-17-edited.json': [{
+        'VIDEO_TYPE_URI': 'VIDEO'
+    }, {
+        'AUTHOR_APPELLATION_URI': 'AUTHOR'
+    }],
+    's09-s-18-artists.json': [{
+        'Death_URI': 'birth_death_date'
+    }, {
+        'artist_appellation_uri': 'name'
+    }, {
+        'Birth_URI': 'birth_death_date'
+    }, {
+        'artist_uri': 'name'
+    }],
+    's10-s-18-artworks.json': [{
+        'auther_uri': 'artist'
+    }, {
+        'object_uri': 'accession_id'
+    }, {
+        'material_uri': 'materials'
+    }, {
+        'author_appellation_uri': 'artist'
+    }],
+    's11-s-19-artists.json': [{
+        'biography_type_uri': 'artist_name' // To mantain clean uri
+    }, {
+        'occupation_type_uri': 'occupation'
+    }],
+    's12-s-19-artworks.json': [{
+        'artist_uri': 'artist_name'
+    }, {
+        'object_uri': 'access_id'
+    }, {
+        'artist_appellation_uri': 'artist_name'
+    }],
+    's13-s-art-institute-of-chicago.json': [{
+        'Death_URI': 'Birthdeathyear'
+    }, {
+        'Artist_URI': 'Artistname'
+    }, {
+        'Artist_Appellation_URI': 'Artistname'
+    }, {
+        'Technique_uri': 'Typeofartwork'
+    }, {
+        'Birth_URI': 'Birthdeathyear'
+    }],
+    's14-s-california-african-american.json': [{
+        'object_uri': 'accessionId'
+    }, {
+        'ethnicity_uri': 'enthnicity'
+    }, {
+        'artist_appellation_uri': 'artist'
+    }],
+    's15-s-detroit-institute-of-art.json': [{
+        'Death_URI': 'death'
+    }, {
+        'Birth_URI': 'birth'
+    }, {
+        'artist_uri': 'name'
+    }, {
+        'artist_appellation_uri': 'name'
+    }, {
+        'object_uri': 'accession'
+    }],
+    's16-s-hammer.json': [{
+        'artist_uri': 'artist'
+    }, {
+        'technique_uri': 'description' // Pay attention to this field
+    }, {
+        'artist_appellation_uri': 'artist'
+    }],
+    's17-s-houston-museum-of-fine-arts.json': [{
+        'Death_URI': 'artistyear'
+    }, {
+        'artist_appellation_uri': 'artist'
+    }, {
+        'Birth_URI': 'artistyear'
+    }],
+    's18-s-indianapolis-artists.json': [{
+        'artist_uri': 'name'
+    }, {
+        'Death_URI': '1889-1975'
+    }, {
+        'artist_appellation_uri': 'name'
+    }, {
+        'Birth_URI': '1889-1975'
+    }],
+    's19-s-indianapolis-artworks.json': [{
+        'material_URI': 'materials'
+    }, {
+        'provenance_type_uri': 'creditLine'
+    }, {
+        'Birth_URI': 'birthDeath'
+    }, {
+        'Death_URI': 'birthDeath'
+    }, {
+        'galleryLabel_type_uri': 'title' // Probably it avoid issues in the URI generation
+    }, {
+        'artist_appellation_uri': 'artist'
+    }],
+    's20-s-lacma.json': [{
+        'Classification_URI': 'Origin' // Pay attention to this field
+    }, {
+        'Artist_Appellation_URI': 'Artist_Name'
+    }, {
+        'Death_URI': 'Artist_Birth_and_Death_Details'
+    }, {
+        'Object_URI': 'ID'
+    }, {
+        'Birth_URI': 'Artist_Birth_and_Death_Details'
+    }, {
+        'Artist_URI': 'Artist_Name'
+    }],
+    's22-s-moca.json': [{
+        'person_uri': 'Artist_Name'
+    }, {
+        'object_uri': 'Assension_Number'
+    }, {
+        'person_appellation_uri': 'Artist_Name'
+    }],
+    's23-s-national-portrait-gallery.json': [{
+        'Object_URI': 'Ref'
+    }, {
+        'Death_URI': 'ArtistBornDiedDate'
+    }, {
+        'Artist_Appellation_URI': 'Artist'
+    }, {
+        'Birth_URI': 'ArtistBornDiedDate'
+    }, {
+        'Subclassification_URI': 'Keywords'
+    }],
+    's24-s-norton-simon.json': [{
+        'Death_URI': 'artist_period'
+    }, {
+        'artist_appellation_uri': 'access'
+    }, {
+        'object_uri': 'access'
+    }, {
+        'Birth_URI': 'artist_period'
+    }],
+    's25-s-oakland-museum-paintings.json': [{
+        'Type_URI': 'Type_of_Art'
+    }, {
+        'Object_uri': 'Accession_id'
+    }, {
+        'Birth_URI': 'BirthDeathDate'
+    }, {
+        'Artist_URI': 'ArtistName'
+    }, {
+        'Subtype_URI': 'Subtype_of_Art'
+    }, {
+        'Artist_Appellation_URI': 'ArtistName'
+    }, {
+        'Death_URI': 'BirthDeathDate'
+    }],
+    's26-s-san-francisco-moma.json': [{
+        'artistAppellationURI': 'artistName'
+    }, {
+        'Death_URI': 'deathYear'
+    }, {
+        'Type_URI': 'type'
+    }, {
+        'Birth_URI': 'birthYear'
+    }, {
+        'artistURI': 'artistName'
+    }],
+    's27-s-the-huntington.json': [{
+        'Death_URI': 'death_date'
+    }, {
+        'artist_appellation_uri': 'artist'
+    }, {
+        'object_uri': 'object_no'
+    }, {
+        'Nationality_URI': 'nationality'
+    }, {
+        'Birth_URI': 'birth_date'
+    }],
+    's28-wildlife-art.json': [{
+        'Death_URI': 'Date2'
+    }, {
+        'Object_URI': 'ID Number'
+    }, {
+        'Collector_Information_typeURI': 'Collector'
+    }, {
+        'Maker_Appellation_URI': 'Maker'
+    }, {
+        'Acquisition_URI': 'How Acquired?' // Check if it is valid
+    }, {
+        'Nationality_URI': 'Place Made'
+    }, {
+        'Birth_URI': 'Date2'
+    }],
+    's29-gilcrease.json': [{
+        'Object_URI': 'ObjectID'
+    }, {
+        'Type_URI': 'Classification'
+    }, {
+        'Attribution_Appellation_URI': 'Attribution'
+    }]
+}
