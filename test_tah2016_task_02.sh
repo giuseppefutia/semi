3c8a2fd2ec9da925174f1ac1318cc0ebe292e379
Giuseppe Futia <giuseppe.futia@gmail.com>
2020-01-08 18:56:55 +0100

    Add test scripts

---
+++ test_tah2016_task_02.sh
@@ -0,0 +1,18 @@
+node run/graph.js \
+data/taheriyan2016/task_02/semantic_types/updated/s01-cb_st.json \
+data/taheriyan2016/task_02/ontology/ontology.ttl \
+rdfs:domain \
+rdfs:range \
+owl:Class \
+data/taheriyan2016/task_02/semantic_models/plausibles/graph/s01-cb
+
+node run/steiner_tree.js \
+data/taheriyan2016/task_02/semantic_types/updated/s01-cb_st.json \
+data/taheriyan2016/task_02/semantic_models/plausibles/graph/s01-cb_graph.json \
+data/taheriyan2016/task_02/semantic_models/steiner/graph/s01-cb
+
+node run/jarql.js \
+data/taheriyan2016/task_02/semantic_types/updated/s01-cb_st.json \
+data/taheriyan2016/task_02/semantic_models/steiner/graph/s01-cb_steiner.json \
+data/taheriyan2016/task_02/ontology/classes.json \
+data/taheriyan2016/task_02/semantic_models/steiner/jarql/s01-cb
---
+++ test_tah2016_task_03.sh
@@ -0,0 +1,7 @@
+node run/graph.js \
+data/taheriyan2016/task_03/semantic_types/updated/s01-cb_st.json \
+data/taheriyan2016/task_03/ontology/ontology.ttl \
+rdfs:domain \
+rdfs:range \
+owl:Class \
+data/taheriyan2016/task_03/semantic_models/plausibles/graph/s01-cb
