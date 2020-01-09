05f34b79fa9e517695207d8fa2be7f2b727311f9
Giuseppe Futia <giuseppe.futia@gmail.com>
2020-01-09 21:03:33 +0100

    Extend scripts for testing

--- test_tah2016_task_03.sh
+++ test_tah2016_task_03.sh
@@ -5,3 +5,10 @@ rdfs:domain \
 rdfs:range \
 owl:Class \
 data/taheriyan2016/task_03/semantic_models/plausibles/graph/s01-cb
+
+# JARQL representing all plausible semantic models of s04-ima-artworks.json
+node run/jarql.js \
+data/taheriyan2016/task_03/semantic_types/updated/s04-ima-artworks_st.json \
+data/taheriyan2016/task_03/semantic_models/plausibles/graph/s04-ima-artworks_graph.json \
+data/taheriyan2016/task_03/ontology/classes.json \
+data/taheriyan2016/task_03/semantic_models/plausibles/jarql/s04-ima-artworks
