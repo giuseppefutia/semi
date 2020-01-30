511870f3a3319c6ba7a5207a015ecb8ccfade9c5
Giuseppe Futia <giuseppe.futia@gmail.com>
2020-01-30 11:23:19 +0100

    [preparation][training][task_04] Add time info to the scripts

--- preparation/taheriyan2016/train_task_04.sh
+++ preparation/taheriyan2016/train_task_04.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+start_all=$(date +%s.%N)
 for i in $(ls data/taheriyan2016/task_04/learning_datasets/)
 do
+    printf "\n"
     echo "Run script for" ${i}
+    start=$(date +%s.%N)
     python src/link_prediction/link_predict.py \
     --directory data/taheriyan2016/task_04/learning_datasets/${i}/ \
     --train data/taheriyan2016/task_04/learning_datasets/${i}/train.nt \
@@ -13,4 +16,17 @@ do
     --graph-batch-size 1000 \
     --n-hidden 100 \
     --graph-split-size 1
+    #--evaluate-every 10 \
+    #--n-epochs 20
+    duration=$(echo "$(date +%s.%N) - $start" | bc)
+    duration_until_now=$(echo "$(date +%s.%N) - $start_all" | bc)
+    execution_time=`printf "%d seconds" $duration`
+    execution_time_until_now=`printf "%d seconds" $duration_until_now`
+    printf "\n"
+    echo "Time for training with background of" ${i} "is" ${execution_time}
+    echo "Time for training until now is" ${execution_time_until_now}
 done
+duration_all=$(echo "$(date +%s.%N) - $start_all" | bc)
+execution_time_all=`printf "%d seconds" $duration_all`
+printf "\n"
+echo "Total time for training data in Task 04 is" ${execution_time_all}
