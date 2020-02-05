06d1e18ee8992f51a0e208c2707081790c6a87a5
Giuseppe Futia <giuseppe.futia@gmail.com>
2020-02-05 12:01:04 +0100

    Move tests in folder

---
+++ test/test_tah2016_task_04.sh
@@ -0,0 +1,7 @@
+# Test on basic refinement
+node run/refinement.js \
+data/taheriyan2016/task_04/semantic_types/updated/armslist_st.json \
+data/taheriyan2016/task_04/learning_datasets/armslist/model_datasets/scores/best/500/score.json \
+data/taheriyan2016/task_04/semantic_models/steiner/graph/armslist_steiner.json \
+data/taheriyan2016/task_04/semantic_models/plausibles/graph/armslist_graph.json \
+data/taheriyan2016/task_04/semantic_models/refined_basic/jarql/armslist
