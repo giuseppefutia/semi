ed33f5ecd455d19f959853bae8ef1e493dd5296c
Giuseppe Futia <giuseppe.futia@gmail.com>
2020-02-17 11:58:45 +0100

    Extend test on task_04

--- test/test_tah2016_task_04.sh
+++ test/test_tah2016_task_04.sh
@@ -1,3 +1,19 @@
+# Test on generation of plausible semantic models
+node run/graph.js \
+data/taheriyan2016/task_04/semantic_types/updated/alaskaslist_st.json \
+data/taheriyan2016/task_04/ontology/ontology.ttl \
+schema:domainIncludes \
+schema:rangeIncludes \
+rdfs:Class \
+data/taheriyan2016/task_04/semantic_models/plausibles/graph/alaskaslist
+
+# Test on JARQL generation of plausible semantic models
+node run/jarql.js \
+data/taheriyan2016/task_04/semantic_types/updated/alaskaslist_st.json \
+data/taheriyan2016/task_04/semantic_models/plausibles/graph/alaskaslist_graph.json \
+data/taheriyan2016/task_04/ontology/classes.json \
+data/taheriyan2016/task_04/semantic_models/plausibles/jarql/alaskaslist
+
 # Test on basic refinement
 node run/refinement.js \
 data/taheriyan2016/task_04/semantic_types/updated/armslist_st.json \
