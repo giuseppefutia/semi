137428f94b78064da82bd582e2e33cb6876e1d40
Giuseppe Futia <giuseppe.futia@gmail.com>
2020-11-26 16:51:53 +0100

    Remove preparation scripts

--- preparation/taheriyan2016/process_semi_sm_ntx.py
+++
@@ -1,7 +0,0 @@
-import networkx as nx
-
-G = nx.nx_agraph.read_dot(
-    'data/taheriyan2016/task_one/semantic_models/plausibles/dot/s01-cb.dot')
-
-print("Nodes in G: ", G.nodes(data=True))
-print("Edges in G: ", G.edges(data=True))
